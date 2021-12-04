5199884aa4f8f4ebea0efb1a504c8c10e9cdd0bc
Tatiana <79145777+Tezim@users.noreply.github.com>
2021-12-04 15:49:27 +0100

    DAY 4 , debugger is hiding all the answers

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,27 @@
+import sk.stuba.DAY4;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String[] content = new String[2000];    // string / integer value depends on input file
+        String[] content = new String[3000];    // string / integer value depends on input file
         StringBuilder path = new StringBuilder(System.getProperty("user.dir"));
         path.append('\\');
-        path.append("input3.txt");   // open file
+        path.append("input4.txt");   // open file
         File f = new File(path.toString());
         Scanner sc = new Scanner(f);
         int i = 0;
         while(sc.hasNext()){
             String data  = sc.next();
-            content[i] = data;
-            i++;
+            if (data != null){
+              content[i] = data;
+            i++;}
         }
-        System.out.println(DAY3.first(content));
-        System.out.println(DAY3.second(content));
+        DAY4 d = new DAY4();
+        d.first(content);
+
 
     }
 }
---
+++ src/sk/stuba/DAY4.java
@@ -0,0 +1,119 @@
+package sk.stuba;
+
+import java.util.HashMap;
+
+public class DAY4 {
+private  int[] tables = new int[100];
+
+    private  Integer[][][] tabs = new Integer[100][5][5];
+
+    public  Integer first(String[] content){
+
+        Integer[] nums;
+
+        for (int a = 0; a < 100;a++)
+            tables[a] = a;
+        int id = 0;
+        int col = 0;
+        int row = 0;
+        nums = parseNums(content[0]);
+        for (int k = 1; k < content.length - 1;k++) {
+            if (content[k] != "\n" && content[k] != " " && content[k] != null) {
+                 if (col == 5){
+                    col = 0;
+                    row++;
+                    }
+                if (row == 5){
+                    id++;
+                    if (id == 100)
+                        break;
+                    col =0;
+                    row = 0;
+                }
+                tabs[id][row][col] = Integer.parseInt(content[k]);
+                col++;
+            }
+        }
+
+        // separate tab in tabs and inputs
+        //for each number in input cycle tabs and set + if its a match
+        // check for bingo
+       for (int l = 0; l < nums.length; l++) {
+           mark(nums[l]); // mark found numbers with zero ... has no effect on sum
+           int f = check();
+           if (f > -1) {
+               System.out.println(calc(f,nums[l]));
+               tables[f] = -1;
+           }
+       }
+        return 0;
+        }
+
+    public  Integer[] parseNums(String s){
+        String[] tmp = s.split(",");
+        Integer[] tnum = new Integer[200];
+        int i = 0;
+        for (String m : tmp){
+            tnum[i] = Integer.parseInt(m) ;
+            i++;
+        }
+        return tnum;
+    }
+
+    public  void mark(Integer s){
+        for (int id = 0; id < 100; id++) {
+            for (int row = 0; row < 5; row++) {
+                for (int column = 0; column < 5; column++) {
+                    if (tabs[id][row][column] != 0 ){
+                    if (tabs[id][row][column].equals(s)) {
+                        tabs[id][row][column] = 0;
+                    }
+                    }
+                }
+            }
+        }
+    }
+
+    public  int check() {
+        for (int id = 0; id < 100; id++) {
+            if (tables[id] != -1){
+            int sum ;
+
+            for (int column = 0; column < 5; column++) {
+                sum = 0;
+                // sum row, stooopid
+                sum += tabs[id][0][column];
+                sum += tabs[id][1][column];
+                sum += tabs[id][2][column];
+                sum += tabs[id][3][column];
+                sum += tabs[id][4][column];
+                if (sum == 0)
+                    return id;
+            }
+                for (int row = 0; row < 5; row++) {
+                    sum = 0;
+                    // sum row, stooopid
+                    sum += tabs[id][row][0];
+                    sum += tabs[id][row][1];
+                    sum += tabs[id][row][2];
+                    sum += tabs[id][row][3];
+                    sum += tabs[id][row][4];
+                    if (sum == 0)
+                        return id;
+                }
+            }
+        }
+        return -1;
+    }
+    public  int calc(int id, int s){
+        int sum = 0;
+            for (int row = 0; row < 5;row++){
+                for (int column = 0; column<5;column++){
+                        sum += tabs[id][row][column];
+
+                }
+            }
+        return sum * s;
+    }
+
+}
